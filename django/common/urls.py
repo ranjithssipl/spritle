2f38a2dde2e6a8d03b11d7bba0bf7ffddf5ab55d
= <ranjithkumar@star-systems.in>
2021-01-14 02:00:27 +0530

    initial

--- django/common/urls.py
+++
@@ -1,6 +0,0 @@
-from users.views import UserViewset
-from rest_framework import routers
-# Defining Router
-common_router = routers.DefaultRouter()
-
-common_router.register(r'create_user', UserViewset)
